8ec3ead95756c06e716c82609ecba9ec099c1398
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-19 21:21:37 -0300

    chore: oracle sql connected

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PowerUp.Data
+{
+    public class AppDbContext : DbContext
+    {   
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        }
+    }
+}
